e9e3d6a067fe1c689fbb79693b16693bfc3e58db
lepex1 <132009118+lepex1@users.noreply.github.com>
2023-05-01 14:32:57 +0300

    Delete events directory

--- events/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = (bot) => {
-    bot
-    .on('ready', ()=>require('./ready')(bot))
-    .on('messageCreate', (message) => require('./messageCreate')(bot, message))
-    .on('interactionCreate', (interaction) => require('./interactionCreate')(bot, interaction))
-};
